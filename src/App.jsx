37490eacec599dc6430145554ed9d8d31eadbb77
juanEulogio <eulogiomarti@wisc.edu>
2024-07-15 13:24:44 -0500

    t1

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import { Container, Row, Col, Button, Image, Card, Carousel } from 'react-bootstrap';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Routes, Navigate } from 'react-router-dom';
+//import { Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 
 function App() {
   
   return ( <div>
 
-<h1>This is home</h1>
+    <h1>This is home</h1>
 
   </div>
   )
