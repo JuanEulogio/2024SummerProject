9487803e24fe27d315d925e1967753f3aefdd43c
juanEulogio <eulogiomarti@wisc.edu>
2024-07-16 15:38:46 -0500

    revert

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,12 @@ import { useState } from 'react'
 import { Container, Row, Col, Button, Image, Card, Carousel } from 'react-bootstrap';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import { Route, Routes, Navigate } from 'react-router-dom';
-//import { BrowserRouter } from 'react-router-dom';
-import Home from './pages/Home.jsx';
+
 
 function App() {
   
   return ( <div>
-
-    <h1>This is home</h1>
-
+    
   </div>
   )
 }
--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react'
-import { Container, Row, Col, Button, Image, Card, Carousel } from 'react-bootstrap';
-
-export default function Home() {
-    return (<div >
-        <h1>This is home</h1>
-
-    </div>);
-}
